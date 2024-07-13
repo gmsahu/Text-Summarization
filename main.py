e7b1e88af9fb21273498de480d8e692384af8736
gmsahu <gmanjari77@gmail.com>
2024-07-13 16:19:12 -0600

    data validation code added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarization.logging import logger
 from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from textSummarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from textSummarization.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from textSummarization.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -18,3 +18,12 @@ except Exception as e:
         raise e
 
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
